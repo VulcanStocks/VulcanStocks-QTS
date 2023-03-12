f669b251eb2cdf3d6bd7f11076c3dc37c1f5ca8f
21ulwe <21ulwe@stockholmscience.se>
2023-03-12 19:50:09 +0100

    -

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,16 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Application.Services;
-using Persistence;
 using Websocket.Client;
-
-var client = new SocketContext();
-
-client.Start();
-
-System.Threading.Thread.Sleep(1000);
-System.Console.WriteLine("Hello");
-
-client.Stop();
 
 
 /*
