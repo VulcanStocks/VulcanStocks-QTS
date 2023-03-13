4b92fbfaf0f18c881da7e149308b9cfea5178161
Uljas Wennström <uljas.wennstrom@gmail.com>
2023-03-13 16:43:26 +0100

    indicators

--- Application/Indicators/Stochastic.cs
+++ Application/Indicators/Stochastic.cs
@@ -7,46 +7,48 @@ using Domain;
 namespace Application.Indicators
 {
     public class Stochastic : ITechnicalIndicator
-{
-    private readonly int _period;
-    private readonly Queue<float> _highs;
-    private readonly Queue<float> _lows;
-
-    public Stochastic(int period)
     {
-        _period = period;
-        _highs = new Queue<float>(period);
-        _lows = new Queue<float>(period);
+        private readonly int _period;
+
+        private readonly Queue<float> _highs;
+
+        private readonly Queue<float> _lows;
+
+        public Stochastic(int period)
+        {
+            _period = period;
+            _highs = new Queue<float>(period);
+            _lows = new Queue<float>(period);
+        }
+
+        public (float, bool) TryGetValue(float price, float volume)
+        {
+            _highs.Enqueue (price);
+            _lows.Enqueue (price);
+
+            if (_highs.Count > _period)
+            {
+                _highs.Dequeue();
+            }
+
+            if (_lows.Count > _period)
+            {
+                _lows.Dequeue();
+            }
+
+            if (_highs.Count < _period || _lows.Count < _period)
+            {
+                return (0f, false);
+            }
+
+            float highestHigh = _highs.Max();
+            float lowestLow = _lows.Min();
+            float current = price;
+
+            float stochastic =
+                (current - lowestLow) / (highestHigh - lowestLow) * 100;
+
+            return (stochastic, true);
+        }
     }
-
-    public (float, bool) TryGetValue(float price, float volume)
-    {
-        _highs.Enqueue(price);
-        _lows.Enqueue(price);
-
-        if (_highs.Count > _period)
-        {
-            _highs.Dequeue();
-        }
-
-        if (_lows.Count > _period)
-        {
-            _lows.Dequeue();
-        }
-
-        if (_highs.Count < _period || _lows.Count < _period)
-        {
-            return (0f, false);
-        }
-
-        float highestHigh = _highs.Max();
-        float lowestLow = _lows.Min();
-        float current = price;
-
-        float stochastic = (current - lowestLow) / (highestHigh - lowestLow) * 100;
-
-        return (stochastic, true);
-    }
-}
-
 }
